18fbb22964f8e89ec624cfc45a5f1ee49f7dd406
ahmadtech199 <ahmadtech199@gmail.com>
2020-12-30 12:59:08 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [startTime, setStartTime] = useState(false)
+  const [count, setCount] = useState('00:00')
+
+  const handlerSubmitStart = () => {
+    console.log('hjh')
+    setStartTime(!startTime)
+
+  }
+
+  const handlerChangeWorkTime = (e) => {
+    setCount(e.target.value);
+    console.log(count)
+  }
+
+  const handlerChangeBreakTime = (e) => {
+    setCount(e.target.value);
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Project 1: Timer App</Text>
+      <Text style={{ paddingTop: 50 }} >{startTime ? { count } : '00:00'}</Text>
+      <View style={styles.viewBtn}>
+        <Button title={startTime ? 'Start' : 'Stop'} onPress={handlerSubmitStart} />
+        <Button title='RESET' />
+      </View>
+      <View style={styles.viewText}>
+        <Text>Work Time in Minutes</Text>
+        <TextInput keyboardType='numeric' placeholder='Enter number only' onChange={handlerChangeWorkTime} value={count} />
+      </View>
+      <View style={styles.viewText} >
+        <Text>Break Time in Minutes</Text>
+        <TextInput keyboardType='numeric' placeholder='Enter number only' onChange={() => handlerChangeBreakTime} />
+      </View>
     </View>
   );
 }
@@ -17,5 +48,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }, viewBtn: {
+    paddingTop: 50,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '50%'
+  }, viewText: {
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '70%',
+    alignItems: 'center'
+  }
 });
